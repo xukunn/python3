3beacaa5b0708a2b4627ae38685a6d1b3e8f0fab
xukunn <xukunn@foxmail.com>
2016-01-06 17:39:43 +0800

    函数基础

--- fun.py
+++ fun.py
@@ -1,6 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from functools import reduce
 
-def byName(t):
-	return t[0]
+函数
+
+
+>>> int(2.9)
+2
+>>> round(25.5)
+26
+>>> float('2.34')
+2.34
+>>> str(1.23)
+'1.23'
+>>> bool('')
+False
+
+#自定义函数
+def myAbs(x):
+	if not isinstance(x,(int,float)):
+		raise TypeError('bad arg type')
+	if x>=0:
+		return x
+	else:
+		return -x
+
+#空函数 pass
+def functionA():
+	pass  #占位用  也可以用在if/else中
+
+if age >= 18:
+    pass
+
+#返回多个值:其实是返回一个tuple ,只是省略了括号()
+
+#导包
+import math
